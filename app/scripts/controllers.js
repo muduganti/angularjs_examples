1c7c9adb9ff4dcdfe64fd1aacd26f8adedbfe662
ramya muduganti <ramya muduganti>
2015-11-24 15:26:44 -0800

    commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+       .module('parkingApp')
+       .controller('parkingCtrl', function($scope){
+
+       	console.log('with in parking ctrl');
+
+       	$scope.cars =[
+       	              {plate:'02ca123'},
+       	              {plate:'02ca345'},
+                      {plate:'02ca789'},
+                      {plate:'02ca011'}
+                     ];
+
+                     $scope.save = function(car){
+                     	$scope.cars.push(angular.copy(car));
+                     	delete $scope.car;
+                     };
+       });
